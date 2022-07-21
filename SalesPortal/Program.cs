b52de2226dff22232bc76229d4e1ba5eb760ae7d
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2022-07-21 14:42:44 -0700

    including Jquery bootgrid

--- SalesPortal/Program.cs
+++ SalesPortal/Program.cs
@@ -4,8 +4,12 @@ builder.Services.AddRazorPages();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
+var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -13,12 +17,21 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    app.UseMvcWithDefaultRoute();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+});
 
 app.UseAuthorization();
 
